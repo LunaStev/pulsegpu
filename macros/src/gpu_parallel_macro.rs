daa7eb30c7686dba6fc947ee593cd8b9aa64fe88
LunaStev <youngjae681@gmail.com>
2025-05-10 22:52:20 +0900

    Stmt Change

--- macros/src/gpu_parallel_macro.rs
+++ macros/src/gpu_parallel_macro.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Ident, Block, parse::{Parse, ParseStream}, Token, LitInt, braced, Result};
+use syn::{parse_macro_input, Ident, parse::{Parse, ParseStream}, Token, LitInt, braced, Result, Stmt};
 
 pub struct GpuParallelInput {
     pub threads: u32,
